ec9f27750172a1e26fc61a4220cd5ee46c9d14ef
betelhem123 <betelhembiruk8@gmail.com>
2025-01-06 09:14:48 +0300

    sis

--- src/home/Banner.jsx
+++ src/home/Banner.jsx
@@ -1,4 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
+import productData from "../products.json"
+import SelectedCategory from '../components/SelectedCategory';
+import { Link } from 'react-router-dom';
+
 const title = (
     <h2>Search One From <span>Thousand</span>of Products</h2>
 )
@@ -19,14 +23,43 @@ const bannerList = [
     ];
     
 const Banner = () => {
+  const[searchinput, setSearchInput] = useState("");
+  const[filteredProducts, setfilteredProducts] = useState(productData);
+//console.log(productData)
+
+
+//search functionality
+const handleSearch = (e) => {
+  const searchTerm = e.target.value; // Get the input value
+  setSearchInput(searchTerm); // Update search input state
+
+  // Filter products based on the search term
+  const filtered = productData.filter((product) =>
+      product.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  setfilteredProducts(filtered); // Update filtered products state
+};
   return (
+  
     <div className='banner-section style-4'>
       <div className='container'>
         <div className='banner-content'>
 {title}
 <form >
-<input type="text" name='search' id='search' placeholder='search your products' />
+  <SelectedCategory select ={"all"}/>
+<input type="text" name='search' id='search' placeholder='search your products' value={searchinput} onChange={handleSearch} />
+<button type='submuit'>
+<i classname="icofont-search"></i>
+</button>
 </form>
+
+<>{desc}</>
+<ul className="lab-ul">
+{
+  searchinput && filteredProducts.map((product, i)=> <li key={i}>
+     <Link to={`/shop/${product.id}`}>{product.name}</Link></li>)
+}
+</ul>
         </div>
       </div>
     </div>
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import Banner from './Banner'
+import HomeCategory from './HomeCategory'
+import CategoryShowCase from './CategoryShowCase'
+import Register from './Register'
 const HOME = () => {
   return (
     <div>
       <Banner/>
+      <HomeCategory/>
+      <CategoryShowCase/>
+ <Register/>
+
     </div>
   )
 }
