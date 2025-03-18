b6e2a73a6429131d762903ae1d06c2e67b9f2929
unknown <betelhem123>
2025-03-18 13:34:11 +0300

    cona

---
+++ src/components/GoogleMap.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const GoogleMap = () => {
+  return (
+    <div className='map-area'>
+        <div className="maps">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3940.708861364109!2d38.7841601758725!3d8.998914389457426!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164b85e9d21532d1%3A0xaa03ce0d1a2ec63d!2zQWxlbWVuZXNoIFBsYXphIHwgQm9sZSBNZWRoYW5pYWxlbSB8IOGKoOGIiOGIneGKkOGIvSDhjZXhiIvhi5sgfCDhiabhiIwg4Yid4Yu14YiQ4YqS4Yqg4YiI4Yid!5e0!3m2!1sen!2set!4v1742220560979!5m2!1sen!2set" ></iframe>
+        </div>
+    </div>
+  )
+}
+
+export default GoogleMap
--- src/contactPage/Contact.jsx
+++ src/contactPage/Contact.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import PageHeader from '../components/PageHeader'
+import GoogleMap from '../components/GoogleMap';
 const subTitle = "Get in touch with us"; const title = "We're Always Eager To Hear From You!"; const conSubTitle = "Get in touch with Contact us"; const conTitle = "Fill The Form Below So We Can Get To Know You And Your Needs Better."; const btnText = "Send our Message";
 
 const contactList = [ { imgUrl: "/src/assets/images/icon/01.png", imgAlt: "contact icon", title: "Office Address", desc: "1201 park street, Fifth Avenue", }, { imgUrl: "/src/assets/images/icon/02.png", imgAlt: "contact icon", title: "Phone number", desc: "+22698 745 632,02 982 745", }, { imgUrl: "/src/assets/images/icon/03.png", imgAlt: "contact icon", title: "Send email", desc: "admin@shopcart.com", }, { imgUrl: "/src/assets/images/icon/04.png", imgAlt: "contact icon", title: "Our website", desc: "www.shopcart.com", }, ];
@@ -32,7 +33,13 @@ const Contact = () => {
 </div>
             </div>
 
-            <div className="col-xl-8 col-lg-7 col-12"></div>
+
+
+
+            <div className="col-xl-8 col-lg-7 col-12">
+
+                <GoogleMap/>
+            </div>
         </div>
     </div>
 </div>
