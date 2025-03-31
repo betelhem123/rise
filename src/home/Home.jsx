583f4524eb6788a3d7bb49c98c8fe60e9572715f
Betelhem123 <betelhembiurk8@gmail.com>
2025-03-31 18:15:26 +0300

    discounted sliding

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Banner from './Banner'
 import HomeCategory from './HomeCategory'
 import CategoryShowCase from './CategoryShowCase'
-import DiscountedItems from './DiscountedItems'
 import LatestAdditions from './latestAdditions'
 import Footer from  '../components/Footer'
 import Landing from './Landing'
@@ -13,7 +12,6 @@ const HOME = () => {
       <Banner/>
       <HomeCategory/>
       <CategoryShowCase/>
-<DiscountedItems/>
 <LatestAdditions/>
 <Footer/>
     </div>
--- src/home/HomeCategory.jsx
+++ src/home/HomeCategory.jsx
@@ -1,89 +1,97 @@
-import React from 'react';
-import {Link} from "react-router-dom";
+import React, { useRef } from 'react';
+import { Link } from "react-router-dom";
+import './HomeCategory.css'; // Make sure the CSS is imported here
 
-const subTitle = "Choose Any Products";
+const subTitle = "Discounted Items";
 const title = "Buy Everything with Us";
 const btnText = "Get Started Now";
 
 const categoryList = [
-{
-imgUrl: 'src/assets/images/category/01.jpg',
-imgAlt: 'category rajibraj91 rajibraj',
-iconName: 'icofont-brand-windows',
-title: 'DSLR Camera',
-},
-{
-imgUrl: 'src/assets/images/category/02.jpg',
-imgAlt: 'category rajibraj91 rajibraj',
-iconName: 'icofont-brand-windows',
-title: 'Shoes',
-},
-{
-imgUrl: 'src/assets/images/category/03.jpg',
-imgAlt: 'category rajibraj91 rajibraj',
-iconName: 'icofont-brand-windows',
-title: 'Photography',
-},
-{
-imgUrl: 'src/assets/images/category/04.jpg',
-imgAlt: 'category rajibraj91 rajibraj',
-iconName: 'icofont-brand-windows',
-title: 'Formal Dress',
-},
-{
-imgUrl: 'src/assets/images/category/05.jpg',
-imgAlt: 'category rajibraj91 rajibraj',
-iconName: 'icofont-brand-windows',
-title: 'Colorful Bags',
-},
-{
-imgUrl: 'src/assets/images/category/06.jpg',
-imgAlt: 'category rajibraj91 rajibraj',
-iconName: 'icofont-brand-windows',
-title: 'Home Decor',
-},
-]
+  {
+    imgUrl: 'src/assets/images/category/01.jpg',
+    imgAlt: 'category image',
+    iconName: 'icofont-brand-windows',
+    title: 'DSLR Camera',
+    discount: '30% OFF',
+  },
+  {
+    imgUrl: 'src/assets/images/category/02.jpg',
+    imgAlt: 'category image',
+    iconName: 'icofont-brand-windows',
+    title: 'Shoes',
+    discount: '25% OFF',
+  },
+  {
+    imgUrl: 'src/assets/images/category/03.jpg',
+    imgAlt: 'category image',
+    iconName: 'icofont-brand-windows',
+    title: 'Photography',
+    discount: '40% OFF',
+  },
+  {
+    imgUrl: 'src/assets/images/category/04.jpg',
+    imgAlt: 'category image',
+    iconName: 'icofont-brand-windows',
+    title: 'Formal Dress',
+    discount: '15% OFF',
+  },
+  {
+    imgUrl: 'src/assets/images/category/05.jpg',
+    imgAlt: 'category image',
+    iconName: 'icofont-brand-windows',
+    title: 'Colorful Bags',
+    discount: '20% OFF',
+  },
+  {
+    imgUrl: 'src/assets/images/category/06.jpg',
+    imgAlt: 'category image',
+    iconName: 'icofont-brand-windows',
+    title: 'Home Decor',
+    discount: '35% OFF',
+  },
+];
+
 const HomeCategory = () => {
+  const sectionRef = useRef(null);
+
   return (
-    <div className='catgory-section style-4 padding-tb'>
-        <div className='container'>
-{}
-
-<div className='section-header text-center'>
-<span className='subtitle'>{subTitle}</span>
-<h2 className='title'>{"title"}</h2>
-</div>
-{/*section card*/ }
-<div className='section-wrapper'>
-    <div className='row g-4 justify-content-center row-cols-md-3 row-cols-sm-2 row-cols-1' >
-        {categoryList.map((val, i)=> (<div key={i} className='col'>
-            <Link to="/shop" className='category-item'>
-            <div className='category-iner'>
-                {/*image thumbnail*/}
-               <div className="category-thumb">
-                <img src={val.imgUrl} alt="" />
-               </div>
-{/* content*/}
-<div className="category-content">
-    <div className="cate-icon">
-        <i className={val.iconName}></i>
-    </div>
-<Link to="/shop"><h6>{val.title}</h6></Link>
-</div>
-                </div>
-                </Link>
-            </div>))}
-    </div>
-    <div className='text-center mt-6'> 
-        <Link to="/shop" className="lab-btn"><span>{btnText}</span></Link>
-    </div>
-</div>
+    <div className='category-section style-4 padding-tb' ref={sectionRef}>
+      <div className='container'>
+        <div className='section-header text-center'>
+          <span className='subtitle'>{subTitle}</span>
+          <h2 className='title'>{title}</h2>
         </div>
-      
+
+        <div className='section-wrapper'>
+          <div className='row g-4 justify-content-center row-cols-md-3 row-cols-sm-2 row-cols-1'>
+            {categoryList.map((val, i) => (
+              <div key={i} className='col'>
+                <Link to="/shop" className='category-item'>
+                  <div className='category-inner'>
+                    <div className="category-thumb">
+                      <img src={val.imgUrl} alt={val.imgAlt} />
+                      <div className="discount-badge">
+                        <span>{val.discount}</span>
+                      </div>
+                    </div>
+                    <div className="category-content">
+                      <div className="cate-icon">
+                        <i className={val.iconName}></i>
+                      </div>
+                      <h6>{val.title}</h6>
+                    </div>
+                  </div>
+                </Link>
+              </div>
+            ))}
+          </div>
+          <div className='text-center mt-5'>
+            <Link to="/shop" className="lab-btn"><span>{btnText}</span></Link>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomeCategory
-
-
+export default HomeCategory;
