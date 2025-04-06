2fa801ae9854d67e9bf89488106a4e787cdb2a97
betelhem123 <betelhembiruk8@gmail.com>
2025-04-06 16:56:50 +0300

    swiper

--- src/shop/SingleProduct.jsx
+++ src/shop/SingleProduct.jsx
@@ -1,106 +1,102 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import PageHeader from '../components/PageHeader';
 import { Swiper, SwiperSlide } from "swiper/react";
 
-
 // Import Swiper styles
 import "swiper/css";
-import {Autoplay} from "swiper/modules"
+import { Autoplay } from "swiper/modules";
 import ProductDisplay from './ProductDisplay';
-const SingleProduct = () => {
-    const [product, setProduct] = useState([]);
-    const {id} = useParams();
-   // console.log(id)
-   useEffect(()=> {
-    fetch("/src/products.json").then(res => res.json()).then(data => setProduct(data))
-   }, []
-)
-const result = product.filter((p)=> p.id === id);
 
-//console.log(result)
+const SingleProduct = () => {
+  const [product, setProduct] = useState([]);
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetch("/src/products.json")
+      .then(res => res.json())
+      .then(data => setProduct(data));
+  }, []);
+
+  const result = product.filter((p) => p.id === id);
+
+  // Add a check to ensure result has the product data before accessing img
+  if (result.length === 0) {
+    return <div>Loading...</div>; // Or a loading spinner
+  }
+
   return (
     <div>
-      <PageHeader title={"OUR SHOP SINGLE"} curPage={"shop/ single product"}/>
-<div className="shop-single padding-tb aside-bg">
-<div className="container">
-    <div className="row justify-content-center">
-<div className="col-lg-8 col-12">
+      <PageHeader title={"OUR SHOP SINGLE"} curPage={"shop/ single product"} />
+      <div className="shop-single padding-tb aside-bg">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-8 col-12">
+              <article>
+                <div className="product-details">
+                  <div className="row align-items-center">
+                    <div className="col-md-6 col-12">
+                      <div className="product-thumb">
+                        <div className="swiper-container pro-single-top">
+                          <Swiper
+                            spaceBetween={30}
+                            slidesPerView={1}
+                            loop={true}
+                            autoplay={{
+                              delay: 2000,
+                              disableOnInteraction: false
+                            }}
+                            modules={[Autoplay]}
+                            navigation={{
+                              prevEl: ".pro-single-prev",
+                              nextEl: ".pro-single-next"
+                            }}
+                          >
+                            {/* Check if img is an array and render accordingly */}
+                            {Array.isArray(result[0].img) ? (
+                              result[0].img.map((img, i) => (
+                                <SwiperSlide key={i}>
+                                  <img src={img} alt={`Product Image ${i + 1}`} />
+                                </SwiperSlide>
+                              ))
+                            ) : (
+                              <SwiperSlide>
+                                <img src={result[0].img} alt="Product Image" />
+                              </SwiperSlide>
+                            )}
+                          </Swiper>
+                          <div className="pro-single-next">
+                            <i className="icofont-rounded-left"> </i>
+                          </div>
+                          <div className="pro-single-prev">
+                            <i className="icofont-rounded-right"> </i>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
 
-<article>
-    <div className="product-details">
-        <div className="row align-items-center">
-<div className="col-md-6 col-12">
-<div className="product-thumb">
-<div className="swiper-container pro-single-top">
-<Swiper className="mySwiper">{
-    
-       result.map((item, i ) => (
-<Swiper
-spaceBetween={30}
-slidesPerView={1}
-loop={"true"}
-autoplay={{
-    delay: 2000,
-    disableOnInteraction: false
-}}
-modules={[Autoplay]}
-navigation={
-    {
-    prevE1: ".pro-single-prev",
-    nextvE1: ".por-single-next",
-}
-}
->
-<div className='mySwiper'>
-<img src={item.img} alt="" />
-</div>
-</Swiper>
-
-       ))}
-      </Swiper>
-<div className="pro-single-next">
-    <i className="icofont-rounded-left"> </i>
-</div>
-<div className="pro-single-prev">
-    <i className="icofont-rounded-right"> </i>
-</div>
-</div>
-</div>
-</div>
-
-
-
-<div className="col-md-6 col-12">
-<div className="post-content">
-
-    <div >
-        {
-            result.map(item => <ProductDisplay key={item.id} item={item}/>
-             
-            )
-        }
-    </div>
-</div>
-</div>
+                    <div className="col-md-6 col-12">
+                      <div className="post-content">
+                        {result.map(item => (
+                          <ProductDisplay key={item.id} item={item} />
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="review">
+                  {/* Review Section */}
+                </div>
+              </article>
+            </div>
+            <div className='col-lg-4 col-12'>
+              {/* Right side content */}
+            </div>
+          </div>
         </div>
+      </div>
     </div>
-    <div className="review">
-        re
-    </div>
-</article>
+  );
+};
 
-
-</div>
-<div className='col-lg-4 col-12'>
-    right side
-</div>
-    </div>
-</div>
-</div>
-
-    </div>
-  )
-}
-
-export default SingleProduct
+export default SingleProduct;
